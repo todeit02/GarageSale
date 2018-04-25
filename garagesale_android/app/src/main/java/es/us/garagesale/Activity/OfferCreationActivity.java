02843005992feb1cab7ebe25580f3d7a92036e98
Tobias Deißler <tobias_christian.deissler@student.reutlingen-university.de>
2018-04-25 13:26:12 +0200

    TextLengthLimiter primera implementación. Referencias de los Views para OfferCreationActivity y layout simplificación.

--- garagesale_android/app/src/main/java/es/us/garagesale/Activity/OfferCreationActivity.java
+++ garagesale_android/app/src/main/java/es/us/garagesale/Activity/OfferCreationActivity.java
@@ -1,9 +1,17 @@
 package es.us.garagesale.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 import es.us.garagesale.R;
+import es.us.garagesale.Src.TextLengthLimiter;
 
 /**
  * Created by Tobias on 18/04/2018.
@@ -11,10 +19,71 @@ import es.us.garagesale.R;
 
 public class OfferCreationActivity extends Activity
 {
+    private static final int maxTitleCharacters = 100;
+    private static final int maxTagsCharacters = 200;
+    private static final int maxDescriptionCharacters = 400;
+
+    private EditText titleEdit = null;
+    private ArrayList<Button> conditionButtons = new ArrayList<>();
+    private EditText tagsEdit = null;
+    private EditText descriptionEdit = null;
+    private View addPhotoButton = null;
+    private View addLocationButton = null;
+    private Button shortDurationButton = null;
+    private Button mediumDurationButton = null;
+    private Button longDurationButton = null;
+    private ArrayList<Button> durationButtons = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_offer);
+
+        Intent leadingIntent = getIntent();
+        String[] offerTags = leadingIntent.getStringArrayExtra("selected_filter_tags");
+
+        findViewReferences();
+        prepareEditTextLimits();
+
+        for(Button listenerSettingButton : conditionButtons)
+        {
+            listenerSettingButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+    }
+
+    private void findViewReferences()
+    {
+        titleEdit = findViewById(R.id.et_title);
+
+        ViewGroup conditionSelectors = findViewById(R.id.ll_condition_selection);
+        for(int i = 0; i < conditionSelectors.getChildCount(); i++)
+        {
+            conditionButtons.add( (Button)conditionSelectors.getChildAt(i) );
+        }
+
+        tagsEdit = findViewById(R.id.et_tags);
+        descriptionEdit = findViewById(R.id.et_description);
+        addPhotoButton = findViewById(R.id.cl_btn_add_photo);
+        addLocationButton = findViewById(R.id.cl_btn_add_location);
+        shortDurationButton = findViewById(R.id.btn_3_days);
+        mediumDurationButton = findViewById(R.id.btn_7_days);
+        longDurationButton = findViewById(R.id.btn_14_days);
+
+        durationButtons.add(shortDurationButton);
+        durationButtons.add(mediumDurationButton);
+        durationButtons.add(longDurationButton);
+    }
+
+    private void prepareEditTextLimits()
+    {
+        titleEdit.addTextChangedListener(new TextLengthLimiter(titleEdit, maxTitleCharacters, this));
+        tagsEdit.addTextChangedListener(new TextLengthLimiter(tagsEdit, maxTagsCharacters, this));
+        descriptionEdit.addTextChangedListener(new TextLengthLimiter(descriptionEdit, maxDescriptionCharacters, this));
     }
 }
