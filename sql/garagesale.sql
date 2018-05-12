6706690764b3fc1868f425bfd090b1fa7f61e77b
Tobias Deißler <tobias_christian.deissler@student.reutlingen-university.de>
2018-05-13 01:30:38 +0200

    Faltó el nuevo SQL.

--- sql/garagesale.sql
+++ sql/garagesale.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 10. Mai 2018 um 16:46
+-- Erstellungszeit: 13. Mai 2018 um 01:29
 -- Server-Version: 10.1.31-MariaDB
 -- PHP-Version: 7.2.4
 
@@ -40,7 +40,9 @@ CREATE TABLE `cards` (
 --
 
 INSERT INTO `cards` (`id`, `cardNum`, `expDate`, `ccv`) VALUES
-(1, '12345', '2018-03-05', '112');
+(1, '12345', '2018-03-05', '112'),
+(5, '1234123412341234', '2021-08-01', '6789'),
+(6, '123456789123456789', '2034-12-01', '789');
 
 -- --------------------------------------------------------
 
@@ -71,32 +73,27 @@ CREATE TABLE `offers` (
   `name` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `description` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `price` float NOT NULL,
-  `startTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `sold` tinyint(1) NOT NULL,
+  `startTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `sold` tinyint(1) NOT NULL DEFAULT '0',
   `seller_username` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `id` int(11) NOT NULL,
-  `state` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `activePeriod` int(11) NOT NULL
+  `state` tinyint(4) NOT NULL,
+  `activePeriod` int(11) NOT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Daten für Tabelle `offers`
 --
 
-INSERT INTO `offers` (`name`, `description`, `price`, `startTime`, `sold`, `seller_username`, `id`, `state`, `activePeriod`) VALUES
-('MacbookPro1', 'es una macbook que. balboa', 1200, '2018-05-02 17:33:28', 0, 'majitoven', 1, 'usado', 3),
-('Iphone', 'iPhone 4 usado', 120, '2018-04-23 18:47:56', 0, 'majitoven', 24, 'usado', 3);
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `offer_tags`
---
-
-CREATE TABLE `offer_tags` (
-  `offer_id` int(11) NOT NULL,
-  `tag` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `offers` (`name`, `description`, `price`, `startTime`, `sold`, `seller_username`, `id`, `state`, `activePeriod`, `latitude`, `longitude`) VALUES
+('MacbookPro1', 'es una macbook que. balboa', 1200, '2018-05-02 17:33:28', 0, 'majitoven', 1, 0, 3, NULL, NULL),
+('Iphone', 'iPhone 4 usado', 120, '2018-04-23 18:47:56', 0, 'majitoven', 24, 0, 3, NULL, NULL),
+('prueba prueba 123', 'Esta es una descripción.', 1.23, '2018-05-12 16:03:21', 0, 'testuser', 52, 2, 3, 0, 0),
+('Prueba con tags, fotos y ubicación!', 'Se vende una prueba. Amo esta app! :)', 7.89, '2018-05-12 16:05:37', 0, 'testuser', 53, 0, 7, 0, 0),
+('Compraos esta app!', 'Esperamos que finalmente funciona todo con la crea', 32.17, '2018-05-12 23:19:51', 0, 'majitoven', 54, 2, 14, 37.3376829, -5.561727899999999),
+('TV con cuadrado verde', 'Se vende una televisión de un ingeniero de Google.', 599.99, '2018-05-12 23:26:07', 0, 'majitoven', 55, 0, 7, 37.4418834, -122.14301950000001);
 
 -- --------------------------------------------------------
 
@@ -120,7 +117,8 @@ CREATE TABLE `persons` (
 --
 
 INSERT INTO `persons` (`username`, `password`, `name`, `email`, `birthDate`, `nationality`, `card_id`, `reputation`) VALUES
-('majitoven', 'miley1823', 'maria ventura', 'mariajose_v13@hotmail.com', '2018-03-05', 'uruguaya', 12345, 0);
+('majitoven', 'miley1823', 'maria ventura', 'mariajose_v13@hotmail.com', '2018-03-05', 'uruguaya', 12345, 0),
+('testuser', 'test6789', 'Testiño Test Testosteron', 'test@test.tr', '1995-10-12', 'Turquía', 5, 0);
 
 -- --------------------------------------------------------
 
@@ -133,6 +131,37 @@ CREATE TABLE `purchases` (
   `buyer_username` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `offer_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `tags`
+--
+
+CREATE TABLE `tags` (
+  `offer_id` int(11) NOT NULL,
+  `tag` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `tags`
+--
+
+INSERT INTO `tags` (`offer_id`, `tag`) VALUES
+(52, 'tag1'),
+(52, 'tag2'),
+(52, 'tag3'),
+(53, 'yolo'),
+(53, 'sw4g'),
+(53, '¿qué?'),
+(53, 'cuatro'),
+(54, 'sevilla'),
+(54, 'oferta'),
+(54, 'estupendo'),
+(54, 'todoFunciona!!!'),
+(55, 'android'),
+(55, 'cam'),
+(55, 'sample');
 
 --
 -- Indizes der exportierten Tabellen
@@ -159,12 +188,6 @@ ALTER TABLE `offers`
   ADD KEY `seller_username` (`seller_username`);
 
 --
--- Indizes für die Tabelle `offer_tags`
---
-ALTER TABLE `offer_tags`
-  ADD KEY `offer_id` (`offer_id`);
-
---
 -- Indizes für die Tabelle `persons`
 --
 ALTER TABLE `persons`
@@ -179,6 +202,12 @@ ALTER TABLE `purchases`
   ADD KEY `buyer_username` (`buyer_username`);
 
 --
+-- Indizes für die Tabelle `tags`
+--
+ALTER TABLE `tags`
+  ADD KEY `offer_id` (`offer_id`);
+
+--
 -- AUTO_INCREMENT für exportierte Tabellen
 --
 
@@ -186,13 +215,13 @@ ALTER TABLE `purchases`
 -- AUTO_INCREMENT für Tabelle `cards`
 --
 ALTER TABLE `cards`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT für Tabelle `offers`
 --
 ALTER TABLE `offers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- Constraints der exportierten Tabellen
@@ -212,10 +241,10 @@ ALTER TABLE `offers`
   ADD CONSTRAINT `offers_ibfk_1` FOREIGN KEY (`seller_username`) REFERENCES `persons` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints der Tabelle `offer_tags`
+-- Constraints der Tabelle `tags`
 --
-ALTER TABLE `offer_tags`
-  ADD CONSTRAINT `offer_tags_ibfk_1` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `tags`
+  ADD CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`offer_id`) REFERENCES `offers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
